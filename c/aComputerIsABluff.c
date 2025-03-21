ba85482387b6a139a7d3b2066824459a1c433c2c
kshitij <kshitij.professional.id@gmail.com>
2025-03-21 09:47:07 +0530

    3

---
+++ c/aComputerIsABluff.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    int i = 10;
+    while (i == 20) {
+
+        printf("A computer is a bluff\n");
+    
+    }
+
+    while ("A" < "B") {
+
+        printf("A Computer is a bluff\n");
+    
+    }
+
+}
